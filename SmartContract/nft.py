173775038237e1bdb7308c583f17165bb83fb8d0
Fidal <fidal15perfect@gmail.com>
2023-04-02 14:43:54 +0530

    update contract

--- SmartContract/nft.py
+++ SmartContract/nft.py
@@ -8,13 +8,13 @@ class NFT(FA2.FA2):
 
 @sp.add_test(name="tests")
 def test():
-    jerry = sp.test_account("Jerry")
-    tom = sp.test_account("Tom")
-    admin = sp.address("tz1hxTwWPfqAAmqp9RiXoWBi1pTLteHD6eaN")
+    goku = sp.test_account("Goku")
+    naruto = sp.test_account("Naruto")
+    admin = sp.address("tz1cHtGRewCVsbFybBtTy6EM8mvSCjdimxqK")
     scenario = sp.test_scenario()
-    scenario.h1("tutorial tests")
+    scenario.h1("NFT contract DECode_Hub")
     nft = NFT(FA2.FA2_config(non_fungible=True), admin=admin, metadata=sp.big_map({"": sp.utils.bytes_of_string(
         "tezos-storage:content"), "content": sp.utils.bytes_of_string("""{"name": "Attribute Contract", "description": "NFT contract for the tutorial"}""")}))
     scenario += nft
-    nft.mint(token_id=0, address=jerry.address, amount=1, metadata=sp.map({"": sp.utils.bytes_of_string(
+    nft.mint(token_id=0, address=goku.address, amount=1, metadata=sp.map({"": sp.utils.bytes_of_string(
         "ipfs://QmV2howLdzPNAtiinAQhiAR4GaGKtWvNPd3weSVjhKWeVC")})).run(sender=admin)
